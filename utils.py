26a874434d030b800407748f3495d0fcadb42028
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-09-07 18:38:24 +0530

    added utils.py - unfinished

---
+++ utils.py
@@ -0,0 +1,19 @@
+import base64
+from typing import List
+
+def convert_image(image_path: str):
+    base64_string = ""
+    with open(image_path,'rb') as f:
+        base64_string = base64.b64encode(f.read()).decode('utf-8')
+    return base64_string
+
+
+def generate_response(image_paths: List[str], user_prompt: str = ""):
+
+    base64_images = [convert_image(x) for x in image_paths]
+
+    instruction_prompt = ""
+    with open('instruction_prompt.txt','r') as f:
+        instruction_prompt = f.read()
+    
+    
