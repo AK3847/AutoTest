515608655dde300b0e049116c8e09e4cc730e361
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-09-07 19:37:07 +0530

    linted code

--- app.py
+++ app.py
@@ -7,36 +7,33 @@ from utils import generate_response
 app = Flask(__name__)
 
 
-UPLOAD_FOLDER = 'test_uploads'
+UPLOAD_FOLDER = "test_uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route('/',methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         uploaded_files = request.files.getlist("images")
-        text_input = request.form.get('text_input','')
+        text_input = request.form.get("text_input", "")
 
         file_paths = []
         for file in uploaded_files:
-            if file.filename != '':
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            if file.filename != "":
+                file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
                 file.save(file_path)
                 file_paths.append(file_path)
-            
-        generate_response(image_paths=file_paths,user_prompt=text_input)
-        response_md = open('response_markdown.md','r')
-        response_md_html = markdown2.markdown(
-            response_md.read()
-        )
-        return render_template('index.html',output_html = response_md_html)
-    return render_template('index.html')
-        
 
-if __name__ == '__main__':
-    app.run(debug=True,host="127.0.0.3")
+        generate_response(image_paths=file_paths, user_prompt=text_input)
+        response_md = open("response_markdown.md", "r")
+        response_md_html = markdown2.markdown(response_md.read())
+        return render_template("index.html", output_html=response_md_html)
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.3")
--- utils.py
+++ utils.py
@@ -9,55 +9,56 @@ load_dotenv()
 
 def convert_image(image_path: str):
     base64_string = ""
-    with open(image_path,'rb') as f:
-        base64_string = base64.b64encode(f.read()).decode('utf-8')
+    with open(image_path, "rb") as f:
+        base64_string = base64.b64encode(f.read()).decode("utf-8")
     return base64_string
+
 
 def delete_images(image_path: str):
     if os.path.exists(image_path):
         os.remove(image_path)
 
-def generate_response(image_paths: List[str], user_prompt: str = ""):
 
+def generate_response(image_paths: List[str], user_prompt: str = ""):
     base64_images = [convert_image(x) for x in image_paths]
 
     instruction_prompt = ""
-    with open('instruction_prompt.txt','r',encoding="utf8") as f:
+    with open("instruction_prompt.txt", "r", encoding="utf8") as f:
         instruction_prompt = f.read()
-    
-    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-    
+
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
     response = client.chat.completions.create(
-        max_tokens = 16000,
-        model = 'gpt-4o-mini',
-        messages = [
+        max_tokens=16000,
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": f"{instruction_prompt}"},
             {
-                "role" : "system",
-                "content" : f"{instruction_prompt}"
-            },
-            {
-                "role" : "user",
-                "content" : [
-                    {"type" : "text", "text" : f"{user_prompt}"},
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": f"{user_prompt}"},
                     *[
                         {
-                            "type" : "image_url" , "image_url" : {
-                                "url" : f"data:image/png;base64,{base64_image}"
-                            }
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/png;base64,{base64_image}"
+                            },
                         }
                         for base64_image in base64_images
-                    ]
-                ]
-            }
-        ]
+                    ],
+                ],
+            },
+        ],
     )
 
     print(response.choices[0].message.content)
-    trim_response = response.choices[0].message.content.replace('```markdown',"").replace('```',"")
-    with open('response_markdown.md','w') as f:
+    trim_response = (
+        response.choices[0]
+        .message.content.replace("```markdown", "")
+        .replace("```", "")
+    )
+    with open("response_markdown.md", "w") as f:
         f.write(trim_response)
 
     for image in image_paths:
         delete_images(image)
-
-    
